4c41480393c405e36e4c55539b95b18d64024b5c
riyatdul <alriyatdul@gmail.com>
2024-10-03 21:55:46 +0700

    add/4-computed-properties

---
+++ src/4-computed-properties/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const count = ref(0)
+const name = ref("Riyat Dul")
+
+const greeting = computed(()=>{
+    console.log("greeting() was executed")
+    return "Hello, " + name.value + "!"
+});
+
+// function greeting() {
+//     console.log("greeting() was executed")
+//     return "Hello, " + name.value + "!"
+// }
+
+</script>
+
+<template>
+    <input type="text" v-model="name" />
+    
+    <h3>{{ greeting }}</h3>
+    <!-- <h3>{{ greeting() }}</h3> -->
+
+    <button @click="count++">Increment - {{ count }}</button>
+</template>
+
