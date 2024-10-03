4b97e00d13c36760b3b23919bc4dfae2d840cff6
riyatdul <alriyatdul@gmail.com>
2024-10-03 14:58:31 +0700

    add/text-interpolation

---
+++ src/1-text-interpolation/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref("Riyat Dul")
+const tall = ref(170)
+</script>
+<template>
+    <h2>Name: {{ name }}</h2>
+    <h3>Tall: {{ tall }}</h3>
+</template>
