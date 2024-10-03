b6a7975a44c9bb4e8645a61ab46b2e2159c09281
riyatdul <alriyatdul@gmail.com>
2024-10-03 22:10:18 +0700

    add/6-conditional-rendering

---
+++ src/5-conditional-rendering/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const isShow = ref(true)
+const awesome = ref(true)
+const type = ref("D")
+
+</script>
+
+<template>
+   <h2 v-if="isShow">This will be shown if isShow <cite>true</cite></h2>
+   <button @click="awesome = !awesome">Toggle</button>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+
+    <!-- another case -->
+    <div v-if="type === 'A'">
+    A
+    </div>
+    <div v-else-if="type === 'B'">
+    B
+    </div>
+    <div v-else-if="type === 'C'">
+    C
+    </div>
+    <div v-else>
+        Not A/B/C
+    </div>
+</template>
