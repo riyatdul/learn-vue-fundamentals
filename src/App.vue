083dc12b60bf93298f4da48e37429e69f1d40fe9
riyatdul <alriyatdul@gmail.com>
2024-10-03 14:35:56 +0700

    refactor/home-page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="85" height="85" />
+    <h1>Welcome Learn <cite>VueJs</cite> Fundamental</h1>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+.container {
+  display: flex;
+  min-height: 100vh;
+  color: green;
+  background-color: aliceblue;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
+
