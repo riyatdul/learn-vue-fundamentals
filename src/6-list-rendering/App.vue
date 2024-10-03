050a3bad99446c0e764006cc42aa68aedca058d2
riyatdul <alriyatdul@gmail.com>
2024-10-03 22:19:09 +0700

    add/6-list-rendering

---
+++ src/6-list-rendering/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+    {
+        id:1,
+        name:"Riyat"
+    },
+    {
+        id:2,
+        name:"Dul"
+    },
+    {
+        id:3,
+        name:"Insan"
+    }
+])
+</script>
+
+<template>
+    <ul v-for="item in items" :key="item.id">
+        <li>{{ item.name }}</li>
+    </ul>
+
+</template>
