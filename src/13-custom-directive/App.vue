56d6714ed2c8cdc05075134bcc09a0e4a3f41a3f
riyatdul <alriyatdul@gmail.com>
2024-10-04 17:41:42 +0700

    add/13-custom-directive

---
+++ src/13-custom-directive/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+const vHighlight = {
+  mounted: (el, bind) => {
+    el.style.fontSize = `${bind.value}px`,
+    el.style.color = 'teal'
+  }
+}
+
+const vParagraph = {
+  mounted: (el) => {
+    el.style.color = 'red'
+  }
+}
+</script>
+
+<template>
+  <p v-highlight="50">This sentence is important!</p>
+  <p v-paragraph>This paragraph is important!</p>
+</template>
