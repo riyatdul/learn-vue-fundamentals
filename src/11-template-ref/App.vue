50e132b82e9f25a8facf229480d2bfe923a23515
riyatdul <alriyatdul@gmail.com>
2024-10-04 15:52:02 +0700

    add/11-template-ref

---
+++ src/11-template-ref/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {onMounted , useTemplateRef} from 'vue';
+
+const search = useTemplateRef('input-search');
+onMounted(() => {
+    search.value.focus()
+})
+</script>
+
+<template>
+    <input ref="input-search" type="text" placeholder="Tipe s">
+</template>
