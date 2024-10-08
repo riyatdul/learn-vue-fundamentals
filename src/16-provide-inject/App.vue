f52cadd88d60fe63637bb371c9da06244b522859
riyatdul <alriyatdul@gmail.com>
2024-10-08 15:46:07 +0700

    add/15-provide-inject

---
+++ src/16-provide-inject/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { provide } from 'vue';
+
+import ChildComponent from './components/ChildComponent.vue';
+provide("message", "hello")
+provide("info", {name:"Riyat Dul", isAdmin:true});
+</script>
+
+<template>
+    <ChildComponent/>
+</template>
---
+++ src/16-provide-inject/components/ChildComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { inject } from 'vue';
+
+
+const message = inject("message");
+const info = inject("info");
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <h2>{{ info.name }} {{ info.isAdmin }}</h2>
+</template>
